0e89bf3a2fb8d562d96c76eb6e97f8dab3f39f56
CJ Sampson <cjsampson41@gmail.com>
2016-03-01 10:51:14 -0600

    Foreach.php revision

--- foreach.php
+++ foreach.php
@@ -2,15 +2,31 @@
 
 // Construct a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string.
 
+$someFunc = 55;
+
+$someArray = [null, 'Hello', 41, [1, 2, 3], $someFunc];
+
+
 foreach($someArray as $item) {
-	echo gettype($item) "\n";
+	echo gettype($item) . "\n";
 }
 
 // Construct a loop that iterates through each value and outputs only values with a type that is scalar.
 
 foreach($someArray as $item) {
-	if( (gettype($item)) == "scalar") {
-		echo $item;
+	if( (is_scalar($item))) {
+		echo $item . "\n";
 	}
 }
 
+// Create a loop that will echo out every value, including those nested in arrays. Output should look like this.
+
+foreach ($someArray as $item) {
+	if(is_array($item)) {
+		foreach($item as $arrayItem) {
+			echo $arrayItem . "\n";
+		}
+	} else {
+		echo $item . "\n";
+	}
+}
