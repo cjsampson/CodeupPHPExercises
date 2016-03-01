c47af3ad2ed0dfca3dcb466a98cdc302310f8596
CJ Sampson <cjsampson41@gmail.com>
2016-03-01 10:12:26 -0600

    Third step Foreach.php

--- foreach.php
+++ foreach.php
@@ -6,3 +6,11 @@ foreach($someArray as $item) {
 	echo gettype($item) "\n";
 }
 
+// Construct a loop that iterates through each value and outputs only values with a type that is scalar.
+
+foreach($someArray as $item) {
+	if( (gettype($item)) == "scalar") {
+		echo $item;
+	}
+}
+
