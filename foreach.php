0529f6b411fa9bf583796d1f1096a0725623117a
CJ Sampson <cjsampson41@gmail.com>
2016-03-01 10:10:17 -0600

    Foreach.php second step

--- foreach.php
+++ foreach.php
@@ -1,2 +1,8 @@
 <?php
 
+// Construct a loop that iterates through each value and outputs its type as either integer, float, boolean, array, null, or string.
+
+foreach($someArray as $item) {
+	echo gettype($item) "\n";
+}
+
