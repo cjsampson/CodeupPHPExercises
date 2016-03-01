69483d9f7765cba84e2749476af2ec35c33b50af
CJ Sampson <cjsampson41@gmail.com>
2016-03-01 15:13:24 -0600

    arithmetic.php first step

--- arithmetic.php
+++ arithmetic.php
@@ -8,16 +8,24 @@ function add($a, $b)
 function subtract($a, $b)
 {
     // Add code here
+    echo ($a - $b);
 }
 
 function multiply($a, $b)
 {
     // Add code here
+    echo ($a * $b);
 }
 
 function divide($a, $b)
 {
     // Add code here
+    echo ($a / $b);
 }
 
 // Add code to test your functions here
+
+add(10,5);
+subtract(10,5);
+multiply(10,5);
+divide(10,5);
