a23d5713c4c2a1d0a29927803d816a78a1bc00fc
CJ Sampson <cjsampson41@gmail.com>
2016-03-01 11:44:50 -0600

    foreach_books.php Book array built

--- foreach_books.php
+++ foreach_books.php
@@ -1,2 +1,43 @@
 <?php
 
+// $numbers = [1 ,2, 3, 4, 5];
+// foreach($numbers as $key => $value) {
+// 	echo "\$value has a \$key of "
+// }
+
+// $studentDetails = [
+// 	"name" => "Jane Doe",
+// 	"age"  => 29
+// ];
+
+// foreach($studentDetails as $key => $value) {
+// 	echo "Student's $key is $value\n";
+// }
+
+$books = [
+	"Super Mind" => [
+		"published" => 2016,
+		"author"	=> "Norman Rosenthal",
+		"pages"		=> 272
+	], 
+
+
+	"Art of Stress Free Living" => [
+		"published" => 2010,
+		"author"	=> "Sukhraj S. Dhillon",
+		"pages"		=> 167
+	],
+
+
+	"How Not to Die" => [
+		"published" => 2015,
+		"author"	=> "Michael Gregor",
+		"pages" 	=> 576
+	],
+
+	"Eloquent JavaScript" => [
+		"published" => 2015,
+		"author"	=> "Marjin Haverbeke",
+		"pages" 	=> 451
+	]
+];
