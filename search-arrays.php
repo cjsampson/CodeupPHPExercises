7b768f6480152f98956a0ff0ccf873f49f934353
CJ Sampson <cjsampson41@gmail.com>
2016-03-02 15:40:28 -0600

    search-arrays second step

--- search-arrays.php
+++ search-arrays.php
@@ -4,3 +4,14 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
+// Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
+
+function findNameInArray($inputString, $arr) {
+	$result = array_search($inputString, $arr);
+	if($result !== false) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
