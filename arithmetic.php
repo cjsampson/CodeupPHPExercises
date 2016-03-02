94198f18c8db55334448829f8deb135457d8b697
CJ Sampson <cjsampson41@gmail.com>
2016-03-02 09:24:35 -0600

    arithmetic.php and follow-along

--- arithmetic.php
+++ arithmetic.php
@@ -1,5 +1,8 @@
 <?php
 
+$a = 20;
+$b = 13;
+
 function add($a, $b)
 {
     return $a + $b;
--- lesson.php
+++ lesson.php
@@ -14,18 +14,37 @@
 
 // echo $a . PHP_EOL;
 
-$a = 3;
-$b = 6;
+// $a = 3;
+// $b = 6;
 
-function changeNums($num1, $num2) {
-	$num1 = 12;
-	$num2 = 42;
+// function changeNums($num1, $num2) {
+// 	$num1 = 12;
+// 	$num2 = 42;
+// }
+
+// echo $a . PHP_EOL;
+// echo $b . PHP_EOL;
+
+// changeNums($a, $b);
+
+// echo $a . PHP_EOL;
+// echo $b . PHP_EOL;
+
+
+$num = 20;
+
+function add($a = 10, $b =15) {
+	if( is_numeric($a) && is_numeric($b)) {
+		return $a + $b;
+	} else {
+		fwrite(STDOUT, "What do you want 'a' to be (number)?");
+		$a = fgets()
+	}
 }
 
-echo $a . PHP_EOL;
-echo $b . PHP_EOL;
+// echo add(3, $num) . PHP_EOL;
+// echo add($num, 4) . PHP_EOL;
+// echo add(3) 	  . PHP_EOL;
+// echo add()		  . PHP_EOL;
 
-changeNums($a, $b);
-
-echo $a . PHP_EOL;
-echo $b . PHP_EOL;
+echo add("Ryan", 22) . PHP_EOL;
