6fd9b60c2c1c88537551752714d5cd39bbcc39a2
CJ Sampson <cjsampson41@gmail.com>
2016-03-02 10:09:06 -0600

    arithmetic.php throwErrorMessage()

--- arithmetic.php
+++ arithmetic.php
@@ -36,3 +36,74 @@ subtract(10,5);
 multiply(10,5);
 divide(10,5);
 remainder(10,5);
+
+// Validate all the arguments, and display an error if the input is not numeric.
+function add($a, $b) {
+	if(is_numeric($a) && is_numeric($b)) {
+		return $a + $b;
+	} else {
+		return "Your inputs mus be numeric"
+	}
+}
+
+function subtract($a, $b) {
+	if(is_numeric($a) && is_numeric($b)) {
+		return $a - $b;
+	} else {
+		return "Your inputs mus be numeric"
+	}
+}
+
+// Validate divide by 0 errors, display error if attempts to divide by 0 are made.
+
+function divide($a, $b) {
+	$message = "";
+		if(is_numeric($a) && is_numeric($b)) {
+			if($b == 0) {
+				return "Cannot divide by zero. Input non-zero number for divisor";
+			}
+			return $a / $b;
+		} else {
+			return "Your inputs mus be numeric"
+		}
+}
+
+function modulus($a, $b) {
+	$message = "";
+		if(is_numeric($a) && is_numeric($b)) {
+			if($b == 0) {
+				return "Cannot divide by zero. Input non-zero number for divisor";
+			}
+			return $a % $b;
+		} else {
+			return "Your inputs mus be numeric"
+		}
+}
+
+// Make the error messages show the values of the arguments.
+
+function modulus($a, $b) {
+	if(is_numeric($a) && is_numeric($b)) {
+		if($b == 0) {
+			// return "Cannot divide by Zero, \$a was {$a} \$b was {$b}";
+			return throwErrorMessage($a, $b, 'divisorError');
+		}
+		return $a % $b;
+	} else {
+		return throwErrorMessage($a, $b);
+	}
+}
+
+
+// Refactor the error messages into their own throwErrorMessage() function, have the other functions use it for error messaging.
+
+function throwErrorMessage($error = 'number') {
+	if($error == "number") {
+		return "Your inputs must be numberix. \$a was {$a} \$b was {$b}";
+	} elseif ($error == "divisorError") {
+		return "Cannot divide by Zero, \$a was {$a} \$b was {$b}";
+	}
+}
+
+
+
