90883a44310f0286063c9767a7560b36ff03c449
CJ Sampson <cjsampson41@gmail.com>
2016-03-03 16:04:24 -0600

    Inital commit contact-parser.php

---
+++ contact-parser.php
@@ -0,0 +1,14 @@
+<?php
+
+// Open contact-parser.php in Sublime and complete the function parseContacts()
+
+function parseContacts($filename)
+{
+    $contacts = [];
+
+    // todo - read file and parse contacts
+
+    return $contacts;
+}
+
+var_dump(parseContacts('contacts.txt'));
