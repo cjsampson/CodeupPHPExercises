064791cc91bea7242455cc6e4bb0971e2185d17d
CJ Sampson <cjsampson41@gmail.com>
2016-03-22 13:34:51 -0500

    rectangle class

---
+++ rectangle.php
@@ -0,0 +1,17 @@
+<?php
+
+class Rectangle {
+	public $width;
+	public $height;
+
+	public function __construct()
+	{
+		$this->width = $width;
+		$this->height = $height;
+	}
+
+	public function area() 
+	{
+		return $this->width * $this->height;
+	}
+}
---
+++ shapes_test.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once 'rectangle.php';
