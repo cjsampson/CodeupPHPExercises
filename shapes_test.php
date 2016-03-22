e1731ed783ea575cfc05ca8f0efe43f4586a5227
CJ Sampson <cjsampson41@gmail.com>
2016-03-22 14:53:55 -0500

    shapes_test

--- shapes_test.php
+++ shapes_test.php
@@ -6,7 +6,5 @@ require_once 'square.php';
 // echo (new Rectangle(3, 25))->area() . PHP_EOL;
 // echo (new Rectangle(3, 35))->area() . PHP_EOL;
 
-echo (new Square(2, 2))->perimeter() . PHP_EOL;
-echo (new Square(2, 6))->perimeter() . PHP_EOL;
-echo (new Square(4, 4))->perimeter() . PHP_EOL;
-echo (new Square(4, 4))->area() . PHP_EOL;
+echo (new Square(2,4))->area() . PHP_EOL;
+echo (new Rectangle(5,7))->perimeter() . PHP_EOL;
